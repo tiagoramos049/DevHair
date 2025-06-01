959a6d6398418b59b83a7ca0ffe4a4576337203c
Tiago <tiagoramos049@gmail.com>
2025-05-31 22:12:11 -0300

    criar métodos na controller para acessar o banco de dados com todas as operações crud, ajustar string de conexão e ajustar a model com uma nova propriedade e criar os construtores

--- DevHair/Controllers/ClientController.cs
+++ DevHair/Controllers/ClientController.cs
@@ -1,5 +1,12 @@
+using Dapper;
 using DevHair.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Xml.Linq;
 
 namespace DevHair.Controllers
 {
@@ -8,40 +15,69 @@ namespace DevHair.Controllers
     public class ClientController : ControllerBase
     {
         private readonly ILogger<ClientController> _logger;
+        private readonly string? _connectionString;
 
-        public ClientController(ILogger<ClientController> logger)
+        public ClientController(ILogger<ClientController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll() 
         {
-            return Ok();
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                const string sql = "SELECT * FROM Clients WHERE IsActive = 1";
+
+                var client = await sqlConnection.QueryAsync<ClientModel>(sql);
+
+                return Ok(client);
+            }
         }
+
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id) 
         {
-            return Ok();
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                string sql = $"SELECT * FROM Clients WHERE Id = {id}";
+
+                var ids = await sqlConnection.QueryFirstAsync<ClientModel>(sql);
+
+                return Ok(ids);
+            }
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(ClientModel client) 
+        public async void Post(ClientModel client) 
         {
-            return NoContent();
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                sqlConnection.ExecuteAsync("INSERT INTO Clients(Name, StartDate, EndDate, IsActive) VALUES(@name, @StartDate, @EndDate, @IsActive)", client);
+            }
         }
 
         [HttpPut]
-        public async Task<IActionResult> Put(int id, ClientModel client)
+        public async void Put(int id, ClientModel client)
         {
-            return NoContent();
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                sqlConnection.Execute("UPDATE Client SET Nome = @Nome, StartDate = @StartDate, EndDate = @EndDate, IsActive = @IsActive WHERE IdCliente = @IdCliente", client);
+            }
         }
 
         [HttpDelete]
-        public async Task<IActionResult> Delete(int id)
+        public async void Delete(int id)
         {
-            return NoContent();
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                var ids = await sqlConnection.ExecuteAsync($"DELETE FROM Client WHERE Id = @Id", new { Id = id });
+            }
         }
 
 
--- DevHair/Models/ClientModel.cs
+++ DevHair/Models/ClientModel.cs
@@ -1,11 +1,24 @@
-﻿namespace DevHair.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DevHair.Models
 {
     public class ClientModel
     {
+        protected ClientModel() {}
+
+        public ClientModel(string name, DateTime startDate, DateTime endDate)
+        {
+            Name = name;
+            StartDate = startDate;
+            EndDate = endDate;
+            IsActive = true;
+        }
+
+
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime EndDate { get; set; }
-        public bool IsActive { get; set; } = true;
+        public bool IsActive { get; set; }
     }
 }
